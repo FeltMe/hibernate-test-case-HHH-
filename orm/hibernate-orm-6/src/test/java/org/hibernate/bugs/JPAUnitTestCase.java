17d47aed201b707ca26c757e125bffb8abb040e9
Oleksandr <sdrozdiuk12@gmail.com>
2024-02-16 14:48:23 +0200

    commit 2

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -36,26 +36,52 @@ public class JPAUnitTestCase {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
-		var cb = (HibernateCriteriaBuilder) entityManager.getCriteriaBuilder();
+		var inputObject = new InputObject();
+		//In my case, they are could be null or not unique
+		inputObject.keys = List.of(
+				new SomeInputKey(UUID.fromString("89ef1d32-a8eb-4bf7-beba-8428907f62eb"), UUID.fromString("7a773a5d-8836-41ff-a869-1c7e80dcfa0f")),
+				new SomeInputKey(UUID.fromString("96f1afaf-cc5b-4076-bf64-8edf3711baab"), UUID.fromString("01bd7ea6-2b52-4b69-82dc-d7b14f47db79")),
+				new SomeInputKey(UUID.fromString("89ef1d32-a8eb-4bf7-beba-8428907f62eb"), UUID.fromString("01bd7ea6-2b52-4b69-82dc-d7b14f47db79"))
+				);
+		final String ref_1 = "ref_1";
+		final String ref_2 = "ref_2";
 
-		JpaCriteriaQuery<Tuple> data1 = cb.createTupleQuery();
-		data1.multiselect( cb.literal( "89ef1d32-a8eb-4bf7-beba-8428907f62eb" ).alias( "ref_1" ), cb.literal( "01bd7ea6-2b52-4b69-82dc-d7b14f47db79" ).alias( "ref_2" ) );
-		List<JpaCriteriaQuery<Tuple>> dataN = new ArrayList<>();
-		JpaCriteriaQuery<Tuple> data2 = cb.createTupleQuery();
-		data2.multiselect( cb.literal( "c184e405-6ce7-4432-8391-adc0edfd6210" ).alias( "ref_1" ), cb.literal( "f26f2355-2d9f-4d5d-8d04-d81c49ddbf26" ).alias( "ref_2" ) );
-		dataN.add( data2 );
+		var cb = (HibernateCriteriaBuilder) entityManager.getCriteriaBuilder();
+		var allData = new ArrayList<JpaCriteriaQuery<Tuple>>();
+
+		for(var item : inputObject.keys) {
+			JpaCriteriaQuery<Tuple> tupleQuery = cb.createTupleQuery();
+			tupleQuery.multiselect( cb.literal( item.ref_1 ).alias( ref_1 ), cb.literal( item.ref_2 ).alias( ref_2 ) );
+			allData.add(tupleQuery);
+		}
 
 		JpaCriteriaQuery<SomeEntity> cq = cb.createQuery( SomeEntity.class );
 
 		//Query group can't be treated as query spec. Use JpaSelectCriteria#getQueryPart to access query group details
-		JpaCteCriteria<Tuple> dataCte = cq.with( cb.unionAll(data1, dataN.toArray( new JpaCriteriaQuery[0] ) ) );
+		JpaCteCriteria<Tuple> dataCte = cq.with( cb.unionAll((JpaCriteriaQuery<Tuple>)allData.toArray()[0], allData.toArray( new JpaCriteriaQuery[0] ) ) );
 
-		JpaRoot<SomeEntity> root = cq.from( SomeEntity.class );
-		var cteJoin = root.join(dataCte);
-		cteJoin.on( cb.and( cb.equal( root.get( "ref_1" ), cteJoin.get( "ref_1" ) ), cb.equal( root.get( "ref_2" ), cteJoin.get( "ref_2" ) ) ) );
+		//JpaRoot<SomeEntity> root = cq.from( SomeEntity.class );
+		//var cteJoin = root.join(dataCte);
+		//cteJoin.on( cb.and( cb.equal( root.get( ref_1 ), cteJoin.get( ref_1 ) ), cb.equal( root.get( ref_2 ), cteJoin.get( ref_2 ) ) ) );
+
+		//var result = entityManager.createQuery(cq).getResultList();
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	public class InputObject {
+		List<SomeInputKey> keys;
+	}
+
+	public class SomeInputKey {
+		public UUID ref_1;
+		public UUID ref_2;
+
+		SomeInputKey(UUID value_1, UUID value_2) {
+			ref_1 = value_1;
+			ref_2 = value_2;
+		}
 	}
 
 	public class SomeEntity {
